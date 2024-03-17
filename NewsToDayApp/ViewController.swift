bebc8cd4664c20ee23d72237efc8f7c048cbac67
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2024-03-17 20:20:27 +0300

    Initial Commit

---
+++ NewsToDayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsToDayApp
+//
+//  Created by Sergey on 17.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
