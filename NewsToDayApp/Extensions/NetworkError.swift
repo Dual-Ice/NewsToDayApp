b8b2614c06a1f36ec5716c2b7f7e91c29b2e3208
Maksim Stogniy <mstogniy@yandex.com>
2024-03-19 23:39:30 +0300

    Add network errors

---
+++ NewsToDayApp/Extensions/NetworkError.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkError.swift
+//  NewsToDayApp
+//
+//  Created by  Maksim Stogniy on 19.03.2024.
+//
+
+import Foundation
+
+enum NetworkError: LocalizedError {
+    case wrongUrl
+    case notFoundData
+    case wrongCode
+}
+
+extension NetworkError {
+    var localizedDescription: String {
+        switch self {
+        case .wrongUrl:
+            return NSLocalizedString("InvalidURL", comment: "")
+        case .notFoundData:
+            return NSLocalizedString("EmptyResponse", comment: "")
+        case .wrongCode:
+            return NSLocalizedString("ErrorCodeResponse", comment: "")
+        }
+    }
+}
