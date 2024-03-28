b231128393ceb8108eade57ad37ed3aa7af646f0
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2024-03-28 23:25:17 +0300

    Add lunchscreen

---
+++ NewsToDayApp/Modules/OnbordingView/Presenter/OnboardingPresenter.swift
@@ -0,0 +1,26 @@
+//
+//  OnboardingPresenter.swift
+//  NewsToDayApp
+//
+//  Created by Sergey on 28.03.2024.
+//
+
+import Foundation
+
+protocol OnboardingPresenterViewProtocol: AnyObject {
+    
+    
+}
+
+protocol OnboardingPresenterProtocol: AnyObject {
+    
+    
+    
+}
+
+class OnboardingPresenter: OnboardingPresenterProtocol {
+    
+    var view: OnboardingPresenterViewProtocol?
+    
+    
+}
